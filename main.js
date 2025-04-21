5727dd0eff1587832a9c8597a4a8deb142b7f621
CodyA1117 <adamscody079@gmail.com>
2025-04-21 08:23:33 -0500

    added new for each loop that loops through the notes array, and for each note calls the assignEvents function.  When key is pressed, it will turn blue, and when key is let up it will turn back to original color.

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ function assignEvents(note){
 }
 
 // Write a loop that runs the array elements through the function
-
+notes.forEach(function(note){
+  assignEvents(note);
+})
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
